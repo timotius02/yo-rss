83f96d7de42fa3113bf85d6a441390f6e533a014
Timotius Sitorus <timsitorus1@gmail.com>
2014-07-26 20:08:26 -0400

    stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,11 +4,15 @@ socket.on('connect', function () {
 	var count = 0;
 
 	var addUser = function(username){
+		if(count >= 5){
+			$('.yo').first().remove();
+		}
 		var yo = $('#template').clone();
 		$('.chat-box').append(yo);
 		yo.addClass('yo').removeAttr('id');
 
-		yo.find('span').replaceWith("<span>"+username+"</span>");;
+		yo.find('span').replaceWith("<span>"+username+"</span>");
+		count++;
 	}
 
 	socket.on('newyo', function(data) {
